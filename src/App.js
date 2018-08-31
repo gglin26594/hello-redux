c7a9a21a9ea098113aae6d42f69bd1c69c819f04
gglin26594 <lin.cityu@gmail.com>
2018-08-31 17:36:02 +1200

    use of connect function

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 //import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {increment} from './actions';
-import {decrement} from './actions';
+import { connect } from "react-redux";
+import { increment } from "./actions";
+import { decrement } from "./actions";
 
 class App extends Component {
   render() {
+    const { increment } = this.props;
+    const { decrement } = this.props;
     return (
-      
-        <div className="container">
-          <h1 className="jumbotro-heading text-center">{this.props.counter}</h1>
-          <p className="text-center">
-            <button onClick={()=>this.props.dispatch(increment())} className="btn btn-primary mr-2" >Increase</button>
-            <button onClick={()=>this.props.dispatch(decrement())} className="btn btn-danger mr-2" >Decrease</button>
-          </p>
-        </div>
-    
+      <div className="container">
+        <h1 className="jumbotro-heading text-center">{this.props.counter}</h1>
+        <p className="text-center">
+          <button onClick={() => increment()} className="btn btn-primary mr-2">
+            Increase
+          </button>
+          <button onClick={() => decrement()} className="btn btn-danger mr-2">
+            Decrease
+          </button>
+        </p>
+      </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     counter: state.anotherReducer
   };
 };
 
-export default connect(mapStateToProps)(App);
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => {dispatch(increment())}
+//   }
+// }
+
+export default connect(
+  mapStateToProps,
+  { increment, decrement }
+)(App);
