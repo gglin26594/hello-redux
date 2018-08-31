75467c9aaba4a3bbc635ac0c3ed6c63dfbc78d9c
gglin26594 <lin.cityu@gmail.com>
2018-08-31 18:04:29 +1200

    applying redux-logger

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducer";
 
 //import { increment, decrement } from "./actions/index";
 import { Provider } from "react-redux";
 
-const store = createStore(rootReducer);
+import { logger } from "redux-logger";
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 const render = () => {
 	ReactDOM.render(
 		<Provider store={store}>
