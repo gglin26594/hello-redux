e0ace1748c14b3bb6b35a0a3e6d7d2459325526e
gglin26594 <lin.cityu@gmail.com>
2018-08-31 14:16:57 +1200

    add increase and decrease action

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import PropTypes from 'prop-types'
 
 class App extends Component {
   render() {
     return (
       
         <div className="container">
-          <h1 className="jumbotro-heading text-center">1</h1>
+          <h1 className="jumbotro-heading text-center">{this.props.value}</h1>
           <p className="text-center">
-            <button className="btn btn-primary mr-2">Increase</button>
-            <button className="btn btn-danger mr-2">Decrease</button>
+            <button className="btn btn-primary mr-2" onClick={this.props.onClick}>Increase</button>
+            <button className="btn btn-danger mr-2" onClick={this.props.onClickDe}>Decrease</button>
           </p>
         </div>
     
     );
   }
+}
+
+App.propType = {
+  value: PropTypes.number 
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,32 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore} from 'redux';
+import reducer from './reducer/counter';
+
+const store = createStore(reducer);
+const render = ()=> {
+    ReactDOM.render(
+        <App 
+            onClick={handleClick} 
+            onClickDe = {handleClickDe}
+            value={store.getState()}/>, document.getElementById('root')
+    );
+}
+
+const handleClick = ()=> {
+    store.dispatch({
+        type: "INCREMENT"
+    });
+};
+
+const handleClickDe = ()=> {
+    store.dispatch({
+        type: "DECREMENT"
+    });
+};
+
+store.subscribe(render);
+
+render();
 registerServiceWorker();
